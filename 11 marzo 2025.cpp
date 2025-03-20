e7dfbcad8ee758936c03edd445819820b58d27e9
brayan-ocko <river23bri@gmail.com>
2025-03-20 13:53:33 -0600

    Add files via upload

---
+++ 11 marzo 2025.cpp
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+    int main (){
+
+int i = 0;
+
+do{
+   printf( "valor de i = %d \n", i);
+   i++;
+   }while (i<3);
+
+
+
+
+
+printf ("Fin");
+
+system ("Pause");
+
+}
